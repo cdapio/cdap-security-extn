13b4ba32f07fd722cd2c25bd3af2f3388a68880a
Rohit Sinha <rsinha@cask.co>
2016-03-13 16:15:38 -0700

    Revert "CDAP-4379 Added Sentry Binding Module"

--- cdap-sentry/cdap-sentry-model/src/main/java/co/cask/cdap/security/authorization/sentry/model/Dataset.java
+++ cdap-sentry/cdap-sentry-model/src/main/java/co/cask/cdap/security/authorization/sentry/model/Dataset.java
@@ -16,8 +16,6 @@
 
 package co.cask.cdap.security.authorization.sentry.model;
 
-import java.util.Objects;
-
 /**
  * Represents the {@link Authorizable.AuthorizableType#DATASET} authorizable in CDAP
  */
@@ -61,22 +59,5 @@ public class Dataset implements Authorizable {
   @Override
   public String getTypeName() {
     return getAuthzType().name();
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    Dataset that = (Dataset) o;
-    return Objects.equals(name, that.name);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(name);
   }
 }
--- cdap-sentry/cdap-sentry-model/src/main/java/co/cask/cdap/security/authorization/sentry/model/Instance.java
+++ cdap-sentry/cdap-sentry-model/src/main/java/co/cask/cdap/security/authorization/sentry/model/Instance.java
@@ -16,8 +16,6 @@
 
 package co.cask.cdap.security.authorization.sentry.model;
 
-import java.util.Objects;
-
 /**
  * Represents the {@link Authorizable.AuthorizableType#INSTANCE} authorizable in CDAP
  */
@@ -61,22 +59,5 @@ public class Instance implements Authorizable {
   @Override
   public String getTypeName() {
     return getAuthzType().name();
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    Instance that = (Instance) o;
-    return Objects.equals(name, that.name);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(name);
   }
 }
