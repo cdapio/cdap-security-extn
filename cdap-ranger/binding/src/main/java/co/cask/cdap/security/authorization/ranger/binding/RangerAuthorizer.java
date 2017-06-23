da919bfab896fccb6fdb74944550f4e4c1afa6b5
Chaoran Wang <superchaoran@gmail.com>
2017-06-23 15:18:02 -0700

    uncomment enforcement

--- cdap-ranger/binding/src/main/java/co/cask/cdap/security/authorization/ranger/binding/RangerAuthorizer.java
+++ cdap-ranger/binding/src/main/java/co/cask/cdap/security/authorization/ranger/binding/RangerAuthorizer.java
@@ -1,5 +1,6 @@
 package co.cask.cdap.security.authorization.ranger.binding;
 
+import co.cask.cdap.proto.element.EntityType;
 import co.cask.cdap.proto.id.EntityId;
 import co.cask.cdap.proto.id.InstanceId;
 import co.cask.cdap.proto.id.NamespaceId;
@@ -9,15 +10,22 @@ import co.cask.cdap.proto.security.Privilege;
 import co.cask.cdap.proto.security.Role;
 import co.cask.cdap.security.spi.authorization.AbstractAuthorizer;
 import co.cask.cdap.security.spi.authorization.AuthorizationContext;
+import co.cask.cdap.security.spi.authorization.UnauthorizedException;
 import org.apache.commons.io.IOUtils;
+import org.apache.ranger.audit.provider.MiscUtil;
 import org.apache.ranger.plugin.audit.RangerDefaultAuditHandler;
+import org.apache.ranger.plugin.policyengine.RangerAccessRequestImpl;
+import org.apache.ranger.plugin.policyengine.RangerAccessResourceImpl;
+import org.apache.ranger.plugin.policyengine.RangerAccessResult;
 import org.apache.ranger.plugin.service.RangerBasePlugin;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.InetAddress;
 import java.net.URL;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -105,80 +113,80 @@ public class RangerAuthorizer extends AbstractAuthorizer {
   @Override
   public void enforce(EntityId entity, Principal principal, Action action) throws Exception {
     System.out.println("## enforce not supported");
-//    if (rangerPlugin == null) {
-//      LOG.info("Authorizer is still not initialized");
-//      throw new RuntimeException("Authorizer is stil not initialized");
-//    }
-//
-//    String userName = getRequestingUser();
-//    String ip = InetAddress.getLocalHost().getHostName();
-//    java.util.Set<String> userGroups = MiscUtil
-//      .getGroupsForRequestUser(userName);
-//    System.out.println("### The group for this user is: " + userGroups);
-//
-//    Date eventTime = new Date();
-//    String accessType = mapToRangerAccessType(action);
-//
-//    boolean validationFailed = false;
-//    String validationStr = "";
-//
-//    if (accessType == null) {
-//      LOG.warn("Unsupported access type. entity=" + entity
-//                 + ", principal=" + principal + ", action=" + action);
-//      validationFailed = true;
-//      validationStr += "Unsupported access type. action=" + action;
-//    }
-//
-//    RangerAccessRequestImpl rangerRequest = new RangerAccessRequestImpl();
-//    rangerRequest.setUser(userName);
-//    rangerRequest.setUserGroups(userGroups);
-//    rangerRequest.setClientIPAddress(ip);
-//    rangerRequest.setAccessTime(eventTime);
-//
-//    RangerAccessResourceImpl rangerResource = new RangerAccessResourceImpl();
-//    rangerRequest.setResource(rangerResource);
-//    rangerRequest.setAccessType(accessType);
-//    rangerRequest.setAction(accessType);
-//    rangerRequest.setRequestData(entity.toString());
-//
-//    if (entity.getEntity() == EntityType.INSTANCE) {
-//      rangerResource.setValue(KEY_INSTANCE, ((InstanceId) entity).getInstance());
-//    } else if (entity.getEntity() == EntityType.NAMESPACE) {
-//      rangerResource.setValue(KEY_INSTANCE, "cdap");
-//      rangerResource.setValue(KEY_NAMESPACE, ((NamespaceId) entity).getNamespace());
-//    } else {
-//      LOG.warn("Unsupported resourceType=" + entity.getEntity());
-//      validationFailed = true;
-//    }
-//
-//
-//    boolean returnValue = true;
-//    if (validationFailed) {
-//      LOG.warn("Validation failed request=" + rangerRequest);
-//      returnValue = false;
-//    } else {
-//      try {
-//        RangerAccessResult result = rangerPlugin.isAccessAllowed(rangerRequest);
-//        if (result == null) {
-//          LOG.error("Ranger Plugin returned null. Returning false");
-//          returnValue = false;
-//        } else {
-//          returnValue = result.getIsAllowed();
-//        }
-//      } catch (Throwable t) {
-//        LOG.error("Error while calling isAccessAllowed(). request="
-//                       + rangerRequest, t);
-//        throw t;
-//      } finally {
-//        if (LOG.isDebugEnabled()) {
-//          LOG.debug("rangerRequest=" + rangerRequest + ", return="
-//                         + returnValue);
-//        }
-//      }
-//    }
-//    if (!returnValue) {
-//      throw new UnauthorizedException(principal, action, entity);
-//    }
+    if (rangerPlugin == null) {
+      LOG.info("Authorizer is still not initialized");
+      throw new RuntimeException("Authorizer is stil not initialized");
+    }
+
+    String userName = getRequestingUser();
+    String ip = InetAddress.getLocalHost().getHostName();
+    java.util.Set<String> userGroups = MiscUtil
+      .getGroupsForRequestUser(userName);
+    System.out.println("### The group for this user is: " + userGroups);
+
+    Date eventTime = new Date();
+    String accessType = mapToRangerAccessType(action);
+
+    boolean validationFailed = false;
+    String validationStr = "";
+
+    if (accessType == null) {
+      LOG.warn("Unsupported access type. entity=" + entity
+                 + ", principal=" + principal + ", action=" + action);
+      validationFailed = true;
+      validationStr += "Unsupported access type. action=" + action;
+    }
+
+    RangerAccessRequestImpl rangerRequest = new RangerAccessRequestImpl();
+    rangerRequest.setUser(userName);
+    rangerRequest.setUserGroups(userGroups);
+    rangerRequest.setClientIPAddress(ip);
+    rangerRequest.setAccessTime(eventTime);
+
+    RangerAccessResourceImpl rangerResource = new RangerAccessResourceImpl();
+    rangerRequest.setResource(rangerResource);
+    rangerRequest.setAccessType(accessType);
+    rangerRequest.setAction(accessType);
+    rangerRequest.setRequestData(entity.toString());
+
+    if (entity.getEntityType() == EntityType.INSTANCE) {
+      rangerResource.setValue(KEY_INSTANCE, ((InstanceId) entity).getInstance());
+    } else if (entity.getEntityType() == EntityType.NAMESPACE) {
+      rangerResource.setValue(KEY_INSTANCE, "cdap");
+      rangerResource.setValue(KEY_NAMESPACE, ((NamespaceId) entity).getNamespace());
+    } else {
+      LOG.warn("Unsupported resourceType=" + entity.getEntityType());
+      validationFailed = true;
+    }
+
+
+    boolean returnValue = true;
+    if (validationFailed) {
+      LOG.warn("Validation failed request=" + rangerRequest);
+      returnValue = false;
+    } else {
+      try {
+        RangerAccessResult result = rangerPlugin.isAccessAllowed(rangerRequest);
+        if (result == null) {
+          LOG.error("Ranger Plugin returned null. Returning false");
+          returnValue = false;
+        } else {
+          returnValue = result.getIsAllowed();
+        }
+      } catch (Throwable t) {
+        LOG.error("Error while calling isAccessAllowed(). request="
+                       + rangerRequest, t);
+        throw t;
+      } finally {
+        if (LOG.isDebugEnabled()) {
+          LOG.debug("rangerRequest=" + rangerRequest + ", return="
+                         + returnValue);
+        }
+      }
+    }
+    if (!returnValue) {
+      throw new UnauthorizedException(principal, action, entity);
+    }
   }
 
   @Override
